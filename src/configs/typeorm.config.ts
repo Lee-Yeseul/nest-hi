6d62a46bf6f00d9df4dcbbeccbf103790f2bebf5
Lee-Yeseul <ssulv3030@gmail.com>
2023-12-15 14:03:37 +0900

    feature : docker file 추가

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -7,7 +7,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
   host: process.env.RDS_HOSTNAME || dbConfig.host,
   port: process.env.RDS_PORT || dbConfig.port,
-  username: process.env.RDS_USERNAME || dbConfig.uername,
+  username: process.env.RDS_USERNAME || dbConfig.username,
   password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DATABASE || dbConfig.database,
 
